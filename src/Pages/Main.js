bfd4674edb926e4ed53b0056f6d2e936246b771a
Nithishkumar Thiruselvam <104024672+NickAtumJunior@users.noreply.github.com>
2022-05-09 19:34:46 +0530

    Update Main.js

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,21 +2,51 @@ import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { Card, List, ListItem,  ListItemText,CardContent } from '@mui/material';
+
+
+
 
 function Main() {
          const[value, setValues] = useState({
              title: "",
              description: ""
          })
+         const[arr,setArr] = useState([])
+
+         const[count,setCount] = useState(0)
+
          const handleChange = (event) => {
            setValues({
              ...value,
              [event.target.name]: event.target.value,
            });
-   
          }
-          const handleButton = () =>{
-            alert(value.title + " " + value.description)
+          const handleAddButton = () =>{
+            let temp = [...arr]
+            let obj = {
+              id: count,
+              title: value.title,
+              description: value.description,
+            }
+            temp.push(obj);
+            setCount(count + 1);
+            setArr(temp);
+            setValues({
+              title: "",
+              description: "",
+            })
+          }
+
+          const sortingValue = (a,b) => {
+               return b.id = a.id;
+          }
+
+          const handleDelete = (todo) => {
+            let temp = [...arr];
+            let index = temp.findIndex((x) => x.id === todo.id);
+            temp.splice(index,1);
+            setArr(temp);
           }
     return (
         
@@ -49,11 +79,36 @@ function Main() {
                        </Grid>
                  </Grid>
 
-                 <div>
-                    <center><Button variant="contained">Clear</Button> <Button variant="contained"  onClick={handleButton}>Add</Button></center>  
+                 <div> 
+                    <br></br>
+                    <center><Button variant="contained"  onClick={handleAddButton}>Add</Button></center>  
                 </div>
-
-             </center>
+                 <center>
+                   <Card>
+                     <CardContent>
+                     <List>
+                      {arr.length === 0 && <span>No task</span>}
+                      {arr.sort(sortingValue).map((todo) => (
+                         <ListItem
+                         key={todo.id}
+                         secondaryAction={
+                           <Button variant="contained" onClick={handleDelete}>Delete</Button>
+                         }
+                       >
+                         <ListItemText
+                            primary={todo.title}
+                            secondary={todo.description} 
+                         />
+   
+                         </ListItem>
+                      ))}
+                     
+                 </List>
+                     </CardContent>
+                   </Card>
+                 
+                 </center>
+          </center>
            
      
     );
